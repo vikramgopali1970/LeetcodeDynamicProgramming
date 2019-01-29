21ea4de78d368948d849905466206518788cad6c
Vikram Gopali <vikramgopali.93@gmail.com>
2019-01-28 18:48:27 -0600

    Best Time to Buy and Sell Stock

---
+++ BestTimeToBuyAndSellStock.java
@@ -0,0 +1,49 @@
+package LeetcodeDynamicProgramming;
+
+public class BestTimeToBuyAndSellStock {
+
+
+    /**
+     * The idea here is to assume that the first day price is the buying price and from the next day keep
+     * checking the price, if it is less than buying price, we update the buying price and if it is more,
+     * then it is a potential selling price and we calculate the profit by checking the max of previos profit
+     * and this profit.
+     */
+    public int maxProfitRecursion(int[] prices) {
+        if(prices.length == 0){
+            return 0;
+        }
+        return this.maxProfitRecurrsion(prices,1,prices[0],0);
+    }
+
+    private int maxProfitRecurrsion(int[] prices,int ind,int buy,int profit){
+        // System.out.println(profit);
+        if(ind == prices.length){
+            return profit;
+        }else{
+            if(prices[ind] <= buy){
+                profit = this.maxProfitRecurrsion(prices,ind+1,prices[ind],profit);
+            }else{
+                profit =this.maxProfitRecurrsion(prices,ind+1,buy,Math.max(profit,prices[ind]-buy));
+            }
+        }
+        return profit;
+    }
+
+    /**
+     * The recurrence relation is that if(prices < buy) potential buy price, and if(prices > buy) it is a potential
+     * sell price and we calculate the profit and update it if it is better than the previous profit.
+     * */
+    public int maxProfitDP(int[] prices){
+        int profit = 0;
+        int buy = prices[0];
+        for(int i=1;i<prices.length;i++){
+            if(prices[i] <= buy){
+                buy = prices[i];
+            }else{
+                profit = Math.max(profit,prices[i]-buy);
+            }
+        }
+        return profit;
+    }
+}
--- Main.java
+++ Main.java
@@ -6,8 +6,15 @@ public class Main {
         /**
          * Leet Code #256: Paint House
          * */
-        PaintHouse ph = new PaintHouse();
-        System.out.println(ph.minCostDP(new int[][]{{8,12,18},{14,6,8},{10,9,13},{2,17,14},{18,18,6},{2,1,15},{19,20,2},{18,15,16},{20,18,18},{15,10,10},{2,20,18},{14,5,15},{18,10,12},{9,17,19},{9,1,6},{4,4,10},{7,8,15},{16,17,4},{16,16,13},{12,7,10},{14,13,8},{16,6,18},{10,5,10},{3,5,11},{9,9,6},{10,15,19},{4,5,19},{12,17,17}}));
+//        PaintHouse ph = new PaintHouse();
+//        System.out.println(ph.minCostDP(new int[][]{{8,12,18},{14,6,8},{10,9,13},{2,17,14},{18,18,6},{2,1,15},{19,20,2},{18,15,16},{20,18,18},{15,10,10},{2,20,18},{14,5,15},{18,10,12},{9,17,19},{9,1,6},{4,4,10},{7,8,15},{16,17,4},{16,16,13},{12,7,10},{14,13,8},{16,6,18},{10,5,10},{3,5,11},{9,9,6},{10,15,19},{4,5,19},{12,17,17}}));
 //        System.out.println(ph.minCostDP(new int[][]{{17,2,17},{16,16,5},{14,3,19}}));
+
+        /**
+         * Leet Code #121: Best Time to Buy and Sell Stock
+         * */
+//        BestTimeToBuyAndSellStock bttbass = new BestTimeToBuyAndSellStock();
+//        System.out.println(bttbass.maxProfitDP(new int[]{7,1,5,3,6,4}));
+
     }
 }
