8a45b675e103fbd6bf49d9a4b6c19b690dc6b8c7
Vikram Gopali <vikramgopali.93@gmail.com>
2019-04-21 14:59:34 -0500

    Number of Enclaves

--- HouseRobber.java
+++ HouseRobber.java
@@ -8,24 +8,10 @@ public class HouseRobber {
      * house in the robbery or not. If included not to include the next house.
      * */
     public int robDP(int[] nums) {
-        if(nums.length == 0){
-            return 0;
-        }else if(nums.length == 1){
-            return nums[0];
-        }
         int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = nums[1];
-        int max = Math.max(dp[0],dp[1]);
         for(int i=2;i<dp.length;i++){
-            dp[i] = nums[i];
-            for(int j=0;j<i-1;j++){
-                dp[i] = Math.max(dp[i],nums[i]+dp[j]);
-            }
-            if(max < dp[i]){
-                max = dp[i];
-            }
+                dp[i] = Math.max(dp[i-1],nums[i-2]+dp[i-2]);
         }
-        return max;
+        return dp[dp.length-1];
     }
 }
